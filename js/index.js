5a21721f838e4b2d65ab131e8e8037738fad741e
yangbao <yangbao1024@163.com>
2022-07-14 11:58:47 +0800

    第一次提交

---
+++ js/index.js
@@ -0,0 +1,9 @@
+(function(window){
+    s = {};
+    s.add = function(pre,next){
+        var sum;  
+        sum = pre + next;
+        return sum
+    }
+    window.$c = s;
+})(window)
---
+++ readme.txt
@@ -0,0 +1,3 @@
+git  项目导入
+welcome to shanxi
+hello  mis
